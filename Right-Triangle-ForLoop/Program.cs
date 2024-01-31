a92be0d189abc448ba712589a8d3801cabc44c2b
janthony25 <junanthony.business@gmail.com>
2024-01-31 20:31:14 +1300

    Add project files.

---
+++ Right-Triangle-ForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightTriangle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Right Triangle Pattern\n\n");
+
+            int row = 5;
+
+            for (int i = 0; i <= row; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
